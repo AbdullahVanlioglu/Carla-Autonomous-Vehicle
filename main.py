176054f793c9b4e728c05b7ff5e36c0807edea30
Abdullah Vanlıoğlu <a.vanli2019@gmail.com>
2020-02-20 21:41:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -200,6 +200,42 @@ class DQNAgent:
         
         X.append(current_state)
         y.append(current_qs)
+
+	    log_this_step = False
+	    if self.tensorboard.step > self.last_logged_episode:
+	    	log_this_step = True
+	    	self.last_logged_episode = self.tensorboard.step
+
+	    with self.graph.as_default():
+	    	self.model.fit(np.array(X)/255, np.array(y), batch_size = TRAINING_BATCH_SIZE, verbose = 0, 
+	    		shuffle = False, callbacks = [self.tensorboard] if log_this_step else None)
+
+	    if log_this_step:
+	    	self.target_update_counter += 1
+
+	    if self.target_update_counter > UPDATE_TARGET_EVERY:
+	    	self.target_model.set_weights(self.model.get_weights())
+	    	self.target_update_counter = 0
+
+	def get_qs(self, state):
+		return self.model.predict(np.array(state).reshape(-1 *state.shape)/255)[0]
+
+
+	def train_in_loop(self):
+		X = np.random.uniform(size = (1, IM_HEIGHT, IM_WIDTH, 3)).astype(np.float32)
+		y = np.random.uniform(size = (1, 3)).astype(np.float32)
+		with self.graph.as_default():
+			self.model.fit(X,y, verbose = False, batch_size = 1)
+
+		self.training_initialized = True
+
+		while True:
+			if self.terminate:
+				return
+			self.train()
+			time.sleep(0.01)
+
+		
         
 		
 
